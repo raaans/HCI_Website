ae3b75c9780532f4c394f2ae3c5069d2fb79fa01
raaans <52825098+raaans@users.noreply.github.com>
2020-05-22 15:43:14 +0200

    added modal functionality

--- _js/modals.js
+++ _js/modals.js
@@ -1,44 +1,22 @@
 //function to show a video or piece of code when the appropriate button is clicked
 $(document).ready(function () {
-
     $(".videoButt").click(function () { //when a button of the class 'videoButt' is clicked...
-        var vButt = $(".videoButt"); //create variable for the video button
-        var video = $(".videoContainer"); //create variable for the video container
-
-        mainNav.removeClass("navVisible");
-        mainNav.addClass("navHidden");
-        subNav.addClass("navVisible");
-        subNav.removeClass("navHidden");
+        $(this).next(".videoModal").addClass("modalVisible");
     });
 
-    $("#workshops").click(function () {
-        var mainNav = $("#mainNav");
-        var subNav = $("#workshopsSubNav");
-
-        mainNav.removeClass("navVisible");
-        mainNav.addClass("navHidden");
-        subNav.addClass("navVisible");
-        subNav.removeClass("navHidden");
+    $(".codeButt").click(function () {
+        $(this).next(".videoModal").addClass("modalVisible");
     });
 
-    $("#projNavBack").click(function () {
-        var mainNav = $("#mainNav");
-        var subNav = $("#projectsSubNav");
-
-        mainNav.removeClass("navHidden");
-        mainNav.addClass("navVisible");
-        subNav.removeClass("navVisible");
-        subNav.addClass("navHidden");
+    $(".modalClose").click(function () {
+        $(this).parent().parent().removeClass("modalVisible");
     });
 
-    $("#workNavBack").click(function () {
-        var mainNav = $("#mainNav");
-        var subNav = $("#workshopsSubNav");
+    $(".modal").click(function () {
+        var target = $(event.target);
 
-        mainNav.removeClass("navHidden");
-        mainNav.addClass("navVisible");
-        subNav.addClass("navHidden");
-        subNav.removeClass("navVisible");
-
+        if (target.is(".modal")) {
+            $(this).removeClass("modalVisible");
+        }
     });
 });
