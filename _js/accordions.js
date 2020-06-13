7aff383449cb1adf29211d0de726e547d97053a5
raaans <52825098+raaans@users.noreply.github.com>
2020-06-13 20:43:27 +0200

    Streamlined accordion.js

--- _js/accordions.js
+++ _js/accordions.js
@@ -1,8 +1,8 @@
 //function to collapse or show the content of a section
 $(document).ready(function () {
     $(".sectionDivider").click(function () { //when a button of the class 'videoButt' is clicked...
-        $(".sectionContent").removeClass("contentVisible");
-        $(".sectionDivider").removeClass("upsideDown");
+        $(".sectionContent").not(this).removeClass("contentVisible");
+        $(".sectionDivider").not(this).removeClass("upsideDown");
         $(this).next(".sectionContent").toggleClass("contentVisible");
         $(this).toggleClass("upsideDown");
     });
