4c90c3610aef0a8c3a4ea25deb0747ce91662581
raaans <52825098+raaans@users.noreply.github.com>
2020-06-13 20:38:25 +0200

    Updated accordions to close others

--- _js/accordions.js
+++ _js/accordions.js
@@ -1,6 +1,8 @@
 //function to collapse or show the content of a section
 $(document).ready(function () {
     $(".sectionDivider").click(function () { //when a button of the class 'videoButt' is clicked...
+        $(".sectionContent").removeClass("contentVisible");
+        $(".sectionDivider").removeClass("upsideDown");
         $(this).next(".sectionContent").toggleClass("contentVisible");
         $(this).toggleClass("upsideDown");
     });
